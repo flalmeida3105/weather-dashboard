cc639d5309267f74bd6edecf6f283375394afe62
Fernando Almeida <nando.lalmeida@gmail.com>
2022-04-06 01:37:02 -0400

    added saved history

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# weather-dashboard
-A website to display temperature for different cities while keeping the data in LocalStorage
+# Fernando Almeida - Weather Dashboard.
+
+## Purpose
+A website that provides a quick way to to check the current temperature of your favorite cities, while presenting a 5 days forecast.
+
+## Built With
+* HTML
+* JS
+* CSS
+
+## Website
+https://github.com/flalmeida3105/weather-dashboard
+
+## About the Website
+A website that provides a quick way to to check the current temperature of your favorite cities, while presenting a 5 days forecast.
+
+The site presents several information about the current weather at the defined location, information such as wind speed, humidity, the actual dates, and a image representing the actual weather, which includes rain, snow, sun, etc.
+
+After searching for a city, the newly searched city will be saved just below the search button, which will facilitate for next queries. Ahh, and don't worry about typing your favorite cities all the time, once searched, it will be locally stored on your computer. 
+
+## Enjoy a quick overview of the website
+<img src="assets/images/weather-dashboard.gif" width="550" height="250" >
+
+
+## Contribution
+Powered by Fernando Almeida
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,9 @@ var getCurrentCityElement = document.querySelector(".current-city");
 var searchCityLocation = document.querySelector(".btn");
 var getCurrentWeatherConditionsElement = document.querySelector(".current-weather-conditions");
 var getForecastInfo = document.querySelector(".future-forecast-info");
+var getCityHistoryElement = document.querySelector(".city-history");
+var getCity = getCityHistoryElement
 var apiKey = "2721e9284e13e8d9c9f8b97f5cb1de42";
-
-// function getCurrentDate() {
-//     // get current date 
-//     var currentDate = moment().format("dddd, MMMM Do");
-//     return currentDate;
-// };
 
 function getCurrentWeather(data) {
     console.log("1", data)
@@ -32,9 +28,9 @@ function getCurrentWeather(data) {
                 var humidity = data.current.humidity;
                 var uvIndex = data.current.uvi;
                 var currentDate = moment().format("(MM/DD/YYYY)");  
-                
-
                 var futureForecast = '';
+
+
                 data.daily.forEach((day, index) => {
                     if(index == 0) {
                         var setCityWeatherImage = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`
@@ -71,37 +67,98 @@ function getCurrentWeather(data) {
     })
 };
 
+function createCityHistoryButtonElement(data) {
+    var city = data[0].name;
+    console.log("CreateCity", getCity, city);
 
+    var createButton = document.createElement("button");
+    createButton.className = "btn-history";
+    createButton.type = "click";
+    createButton.innerHTML = city;
+    createButton.addEventListener("click", getButtonHistoryClickHandler, data);
+    getCity.appendChild(createButton);
 
+    saveCityHistory(city);
+};
 
+function getButtonHistoryClickHandler() {
+    var city = this.innerHTML;
+    console.log(city)
+    getButtonClickHandler(city);
 
+}
 
-
-function getButtonClickHandler() {
+function getButtonClickHandler(city) {
+    var newCity = city
+    console.log("this ", newCity);
     var getCity = getCityLocationElement.value;
     console.log(getCity);
 
-    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + getCity + "&limit=5&appid=" + apiKey;
-    console.log(apiUrl);
+    if (getCity) {
+        var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + getCity + "&limit=5&appid=" + apiKey;
+        console.log(apiUrl);
 
-    fetch(apiUrl)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    getCurrentWeather(data);
-                });
-            }
-        })
+        fetch(apiUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        getCurrentWeather(data);
+                        createCityHistoryButtonElement(data);
+                    });
+                }
+            })
+    } else {
+        var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + newCity + "&limit=5&appid=" + apiKey;
+        console.log(apiUrl);
 
-
-    // console.log("This is from Input text: " + apiUrl);
-    // getCity;
-
-
+        fetch(apiUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        getCurrentWeather(data);
+                    });
+                }
+            })
+    }
 };
 
+
+function saveCityHistory(city) {
+    var cityObj = {
+        name: city
+    }
+    console.log("CityObj", cityObj)
+
+    var savedHistory = localStorage.getItem("cities");
+    savedHistory = JSON.parse(savedHistory);
+
+    if (!savedHistory) {
+        savedHistory = [cityObj];
+    } else { 
+        savedHistory.push(cityObj);
+        for (var i = 0; i < savedHistory.length; i++) {
+            localStorage.setItem("cities", JSON.stringify(savedHistory[i]));
+        }
+    }
+    localStorage.setItem("cities", JSON.stringify(savedHistory));
+};
+
+function loadCityHistory() {
+ 
+    var savedHistory = localStorage.getItem("cities");
+    savedHistory = JSON.parse(savedHistory);
+    console.log("CityObj", savedHistory)
+
+    for (cities in savedHistory) {
+        var createButton = document.createElement("button");
+        createButton.className = "btn-history";
+        createButton.type = "click";
+        createButton.innerHTML = savedHistory[cities].name;
+        createButton.addEventListener("click", getButtonHistoryClickHandler, savedHistory[cities].name);
+        getCity.appendChild(createButton);
+    }
+}
+
+
 searchCityLocation.addEventListener("click", getButtonClickHandler);
-
-// getButtonClickHandler()
-
-// getCurrentWeather();
+loadCityHistory();
